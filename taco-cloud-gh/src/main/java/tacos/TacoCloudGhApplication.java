564982b12958cb00e95f17a91b652ec635df6bae
lisicicd <44380882+lisicicd@users.noreply.github.com>
2018-11-19 12:28:21 +0100

    Initial (empty) Spring projecc. First commit.

---
+++ taco-cloud-gh/src/main/java/tacos/TacoCloudGhApplication.java
@@ -0,0 +1,12 @@
+package tacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoCloudGhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacoCloudGhApplication.class, args);
+	}
+}
